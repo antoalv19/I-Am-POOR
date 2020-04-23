7b59c731d05dbbf4845f7f5cdea3796d50bddfac
Antonio <antonioalvino19@gmail.com>
2020-04-23 19:31:47 +0200

    Initial Commit

---
+++ I Am POOR/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am POOR
+//
+//  Created by Antonio Alvino on 23/04/2020.
+//  Copyright © 2020 Antonio Alvino. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
